d40379adcc499cdb7faa690199bdc4abc23741fd
Neugodnikova <Neugodnikova.ae@phystech.edu>
2023-05-01 17:18:43 +0500

    final work done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,19 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let client;
+    let data = {client, 
+                order: {address: address, sum: sum}, 
+                goods: []
+    };
     let countOfGoods = goods.length;
+    
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title:goods[i].title, count:goods[i].count})
+    };
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.client = `${name} ${phone}`;
+    let jsonData = JSON.stringify({data});
     return jsonData;
 }
